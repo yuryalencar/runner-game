c17087ef6808194a5d99b4dd6595e45b7249e1b9
Yury Alencar <yuryalencar19@gmail.com>
2018-11-19 00:48:09 -0200

    ADD: Scenario Modularization

--- js/scenario/scenario.js
+++ js/scenario/scenario.js
@@ -2,13 +2,28 @@ class Scenario {
     
     constructor(level){
         this.level = level;
+        this.backgroundSpeed = 0;
+        this.backgroundParallaxSpeed = 0;
+    
+        this.configureSizeBackgroundElement('background', 'arts/scenario/background/level_'+level+'/background.png');
+        this.configureSizeBackgroundElement('backgroundParallax', 'arts/scenario/elements/level_'+level+'/backgroundParallax.png');
     }
 
-    getScenario(){
-        return this.keyUp;
+    configureSizeBackgroundElement(idElement, pathImage){
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+
+        document.getElementById(idElement).style.backgroundImage = "url("+pathImage+")";
+        document.getElementById(idElement).style.width = width + "px";
+        document.getElementById(idElement).style.height = height + "px";    
     }
 
-    getPlayerPath(){
-        return this.playerPath;
+    parallaxScrollingEfect(backgroundSpeed, backgroundParallaxSpeed){
+        this.backgroundSpeed -= backgroundSpeed;
+        this.backgroundParallaxSpeed -= backgroundParallaxSpeed;
+
+        document.getElementById('background').style.backgroundPosition = this.backgroundSpeed + "px 0";
+        document.getElementById('backgroundParallax').style.backgroundPosition = this.backgroundParallaxSpeed + "px 0";
     }
+
 }
